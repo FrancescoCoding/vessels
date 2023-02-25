aa6165f07c4543a27db71232b7e93a52a5c26457
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2023-02-25 23:53:09 +0000

    First upload #RGUHack 💀 2023

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import Map from "./Map";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Map />
+        <a
+          className="App-link"
+          href="https://github.com/RGUCode/Hack2023"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          #RGUHack 2023
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
